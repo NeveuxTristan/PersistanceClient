b49d6cafdd519029d9cbd0767a9fbadf19e239a0
Tristan Neveux <tristan.neveux_du_geniebre@intra.bodet-timesport.com>
2019-04-08 16:44:30 +0200

    Ajout de la page de gestion des visites par users

---
+++ app/src/main/java/iia/tristan/persistanceclient/client/ManagerVisitesActivity.java
@@ -0,0 +1,11 @@
+package iia.tristan.persistanceclient.client;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Neveux_du_Geniebre on 08/04/2019.
+ */
+public class ManagerVisitesActivity extends AppCompatActivity {
+
+
+}
