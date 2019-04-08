07100e16a7bbdb7faa05dfe79c3f416f2e68e8ae
Tristan Neveux <tristan.neveux_du_geniebre@intra.bodet-timesport.com>
2019-04-08 17:02:28 +0200

    Ajout item visite pour manager

---
+++ app/src/main/java/iia/tristan/persistanceclient/client/item/ManagerVisiteItem.java
@@ -0,0 +1,9 @@
+package iia.tristan.persistanceclient.client.item;
+
+import java.io.Serializable;
+
+/**
+ * @author Neveux_du_Geniebre on 08/04/2019.
+ */
+public class ManagerVisiteItem implements Serializable {
+}
