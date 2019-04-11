b8e66a1a7d185e4b6c04ec71f340071e1987ebfb
Tristan Neveux <tristan.neveux_du_geniebre@intra.bodet-timesport.com>
2019-04-11 15:24:43 +0200

    Ajout de la pp màj visite

--- app/src/main/java/client/VisiteActivity.java
+++ app/src/main/java/client/VisiteActivity.java
@@ -1,24 +1,17 @@
 package client;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import client.adapter.VisiteAdapter;
 import client.connection.ConnectionManager;
 import iia.tristan.persistanceclient.R;
-import share.dataObject.Visite;
 import share.manager.DataManager;
 
 public class VisiteActivity extends AppCompatActivity {
 
     private ListView listVisites;
-
-    private Visite itemSelected;
-    private int idItemSelected = -1;
-    private View lastViewSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,23 +20,7 @@ public class VisiteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_visite);
 
         listVisites = findViewById(R.id.list_visite);
-
-        final Visite[] items = (Visite[]) DataManager.INSTANCE.getAllVisiteByUser(ConnectionManager.INSTANCE.getUser()).toArray();
-
-        listVisites.setAdapter(new VisiteAdapter(getApplicationContext(), items));
-
-        listVisites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                selectItem(view, position);
-            }
-        });
-    }
-
-    private void selectItem(View view, int position)
-    {
-
+        listVisites.setAdapter(new VisiteAdapter(getApplicationContext(), DataManager.INSTANCE.getAllVisiteByUser(ConnectionManager.INSTANCE.getUser())));
     }
 
 }
--- app/src/main/java/client/adapter/VisiteAdapter.java
+++ app/src/main/java/client/adapter/VisiteAdapter.java
@@ -1,14 +1,24 @@
 package client.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +26,7 @@ import iia.tristan.persistanceclient.R;
 import share.dataObject.Magasin;
 import share.dataObject.Visite;
 import share.enumUtils.EnumEnseigne;
+import share.enumUtils.EnumVille;
 import share.manager.DataManager;
 
 /**
@@ -24,13 +35,16 @@ import share.manager.DataManager;
 public class VisiteAdapter extends ArrayAdapter implements View.OnClickListener {
 
     private final Context context;
-    private final Visite[] values;
+    private final ArrayList<Visite> values;
 
-    public VisiteAdapter(Context context, Visite[] values)
+    private Dialog dialog;
+
+    public VisiteAdapter(Context context, ArrayList<Visite> values)
     {
         super(context, -1, values);
         this.context = context;
         this.values = values;
+        this.dialog = new Dialog(context);
     }
 
     private ImageButton btnDetails;
@@ -50,7 +64,7 @@ public class VisiteAdapter extends ArrayAdapter implements View.OnClickListener 
 
         btnDetails.setOnClickListener(this);
 
-        int idMagasin = values[position].getIdMagasin();
+        int idMagasin = values.get(position).getIdMagasin();
         Magasin magasin = null;
         for (Magasin m : DataManager.INSTANCE.getAllMagasins())
         {
@@ -66,10 +80,10 @@ public class VisiteAdapter extends ArrayAdapter implements View.OnClickListener 
             visiteIco.setImageResource(EnumEnseigne.CARREFOUR.equals(magasin.getEnseigne()) ? R.drawable.carrefour :
                     EnumEnseigne.LECLERC.equals(magasin.getEnseigne()) ? R.drawable.leclerc : R.drawable.super_u);
 
-            txtVisiteDate.setText(values[position].getDateVisite());
+            txtVisiteDate.setText(values.get(position).getDateVisite());
             txtVisiteName.setText(magasin.getDisplayName());
-            txtVisiteState.setText(values[position].isVisiteDone() ? "Visit TO DO" : "Visit DONE");
-            txtVisiteState.setTextColor(values[position].isVisiteDone() ? Color.RED : Color.GREEN);
+            txtVisiteState.setText(values.get(position).isVisiteDone() ? "Visit TO DO" : "Visit DONE");
+            txtVisiteState.setTextColor(values.get(position).isVisiteDone() ? Color.RED : Color.GREEN);
         }
 
         return itemView;
@@ -80,8 +94,64 @@ public class VisiteAdapter extends ArrayAdapter implements View.OnClickListener 
     {
         if (view == btnDetails)
         {
-
+            showPpSellerVisite(values.get(((ListView) view.getParent()).getPositionForView(view)));
         }
+    }
+
+    private void showPpSellerVisite(final Visite visite)
+    {
+        // Init Pp
+        dialog.setContentView(R.layout.pp_seller_visite);
+        final MaterialButton btnUpdate, btnCancel;
+        final ImageView imageEnseigne;
+        final TextView textVille;
+        final DatePicker datePicker;
+        final Switch stateVisit;
+        final EditText comment;
+
+        btnCancel = dialog.findViewById(R.id.pp_seller_visit_btn_cancel);
+        btnUpdate = dialog.findViewById(R.id.pp_seller_visit_btn_create);
+        imageEnseigne = dialog.findViewById(R.id.pp_seller_visit_icon_enseigne);
+        textVille = dialog.findViewById(R.id.pp_seller_visit_display_ville);
+        datePicker = dialog.findViewById(R.id.pp_seller_visit_date_picker);
+        stateVisit = dialog.findViewById(R.id.pp_seller_visit_switch_state);
+        comment = dialog.findViewById(R.id.pp_seller_visit_comment);
+
+        Magasin m = DataManager.INSTANCE.getMagasinById(visite.getIdMagasin());
+        imageEnseigne.setImageResource(
+                EnumEnseigne.CARREFOUR.equals(m.getEnseigne()) ? R.drawable.carrefour :
+                        EnumEnseigne.LECLERC.equals(m.getEnseigne()) ? R.drawable.leclerc : R.drawable.super_u);
+        textVille.setText(EnumVille.getVilleById(m.getVille()).getDisplayName());
+
+        String dateFormat[] = visite.getDateVisite().split("/");
+        datePicker.updateDate(Integer.valueOf(dateFormat[2]), Integer.valueOf(dateFormat[1]), Integer.valueOf(dateFormat[0]));
+        datePicker.setEnabled(false);
+
+        stateVisit.setChecked(visite.isVisiteDone());
+        comment.setText(visite.getComment().length() > 0 ? visite.getComment() : "Enter comment here...");
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // TODO Save new visit State
+                visite.setVisiteDone(stateVisit.isChecked());
+                visite.setComment(comment.getText().toString());
+                // TODO Save new visit State
+                dialog.dismiss();
+            }
+        });
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 
 }
